6645bf24f00446e7a7d6565d2b8989d2acad2c04
lupashevskyi <47347268+lupashevskyi@users.noreply.github.com>
2020-03-10 18:20:03 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,8 +5,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using MySql.Data.MySqlClient;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 
 namespace robot_authentification
 {
